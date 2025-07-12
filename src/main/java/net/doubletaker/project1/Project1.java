15e1d3e217dbdcf27dfc394cfb96dc8b4b5b1ed2
Doubletaker <doubletakerplayz@gmail.com>
2025-07-12 12:19:39 -0400

    update

---
+++ src/main/java/net/doubletaker/project1/Project1.java
@@ -0,0 +1,17 @@
+package net.doubletaker.project1;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Project1 implements ModInitializer {
+	public static final String MOD_ID = "project1";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/doubletaker/project1/Project1ModClient.java
@@ -0,0 +1,10 @@
+package net.doubletaker.project1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Project1ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
